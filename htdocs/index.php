c0c436bb7f586d7e85e9217bf1e2c576edbf3e67
Elisa Demanez <demanez.elisa@gmail.com>
2018-01-30 10:13:03 +0100

    1st commit

---
+++ htdocs/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require dirname(__FILE__).'/cms/wp-blog-header.php';
+
+?>
+ 
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ lkhouhoh
+ </body>
+ </html>
